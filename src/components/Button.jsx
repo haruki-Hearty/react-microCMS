685026ca05bf90103ef00a6a0e97078cc0c6f338
haruki <haruki1015go@gmail.com>
2024-04-20 11:19:04 +0900

    [fix]cssからscssに変 更

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styles from "../styles/Button.module.css";
+import styles from "../styles/Button.module.scss";
 
 export const Button = (props) => {
   return (
