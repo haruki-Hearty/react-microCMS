ca74f6bdbe9a3ab340ed6d7520c2f8eb9b08a0e7
haruki <haruki1015go@gmail.com>
2024-04-21 22:48:04 +0900

    [add]header完成

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,34 +3,64 @@ import styles from "../styles/Nav.module.scss";
 import { Link } from "react-router-dom";
 
 export const Nav = () => {
-  const [navIsOpen, setNavIsOpen] = useState(false)
+  const [navIsOpen, setNavIsOpen] = useState(false);
 
   const toggleNav = () => {
-    setNavIsOpen((prev) => !prev)
-  }
+    setNavIsOpen((prev) => !prev);
+  };
+  
+  const closeNav = () => {
+    setNavIsOpen(false);
+  };
   return (
     <nav className={navIsOpen ? styles.open : styles.close}>
-      <button className={styles.menuButton} onClick={toggleNav}>menu</button>
+      {navIsOpen && (
+        <style jsx global>{`
+          @media (max-width: 768px) {
+            body {
+              overflow: hidden;
+              position: fixed;
+              width: 100%;
+            }
+          }
+        `}</style>
+      )}
+      <button className={styles.menuButton} onClick={toggleNav}>
+        <span className={styles.bar}></span>
+        <span className="sr-only">menu</span>
+      </button>
       <ul className={styles.lists}>
-          <li className={styles.list}>
-            <Link to="/">HOME</Link>
-          </li>
-          <li className={styles.list}>
-            <Link to="/ABOUT">ABOUT</Link>
-          </li>
-          <li className={styles.list}>
-            <Link to="/SERVICE">SERVICE</Link>
-          </li>
-          <li className={styles.list}>
-            <Link to="/BLOG">BLOG</Link>
-          </li>
-          <li className={styles.list}>
-            <Link to="/WORKS">WORKS</Link>
-          </li>
-          <li className={styles.list}>
-            <Link to="/CONTACT">CONTACT</Link>
-          </li>
-        </ul>
+        <li className={styles.list}>
+          <Link onClick={closeNav} to="/">
+            HOME
+          </Link>
+        </li>
+        <li className={styles.list}>
+          <Link onClick={closeNav} to="/ABOUT">
+            ABOUT
+          </Link>
+        </li>
+        <li className={styles.list}>
+          <Link onClick={closeNav} to="/SERVICE">
+            SERVICE
+          </Link>
+        </li>
+        <li className={styles.list}>
+          <Link onClick={closeNav} to="/BLOG">
+            BLOG
+          </Link>
+        </li>
+        <li className={styles.list}>
+          <Link onClick={closeNav} to="/WORKS">
+            WORKS
+          </Link>
+        </li>
+        <li className={styles.list}>
+          <Link onClick={closeNav} to="/CONTACT">
+            CONTACT
+          </Link>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
