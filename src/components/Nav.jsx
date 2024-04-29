01ee744d4f5f2d26037a41dac1a2b07821d4cfb8
haruki <haruki1015go@gmail.com>
2024-04-29 17:18:41 +0900

    [fix]不要なコメントを削除

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,8 +13,6 @@ export const Nav = () => {
     }
   }, [navIsOpen]);
 
-  // 最初に考えた配列　リンクをどうするかで下の配列に変更
-  // const linkPages = ["HOME", "ABOUT", "SERVICE", "BLOG", "WORKS", "CONTACT"];
   const linkPages = [
     {
       path: "/",
@@ -65,36 +63,6 @@ export const Nav = () => {
             <Link onClick={closeNav} to={page.path}>{page.pageName}</Link>
           </li>
         ))}
-        {/* <li className={styles.list}>
-          <Link onClick={closeNav} to="/">
-            HOME
-          </Link>
-        </li>
-        <li className={styles.list}>
-          <Link onClick={closeNav} to="/ABOUT">
-            ABOUT
-          </Link>
-        </li>
-        <li className={styles.list}>
-          <Link onClick={closeNav} to="/SERVICE">
-            SERVICE
-          </Link>
-        </li>
-        <li className={styles.list}>
-          <Link onClick={closeNav} to="/BLOG">
-            BLOG
-          </Link>
-        </li>
-        <li className={styles.list}>
-          <Link onClick={closeNav} to="/WORKS">
-            WORKS
-          </Link>
-        </li>
-        <li className={styles.list}>
-          <Link onClick={closeNav} to="/CONTACT">
-            CONTACT
-          </Link>
-        </li> */}
       </ul>
     </nav>
   );
