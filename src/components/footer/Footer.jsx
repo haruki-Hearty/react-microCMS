91fb93714d2e6aaa82e7033a88b1af2804c9ea7c
haruki <haruki1015go@gmail.com>
2024-05-06 17:47:35 +0900

    [add]footer完成

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,27 @@
+import { GithubIconSvg, InstaIconSvg, XIconSvg } from "../svg/SocialSvg";
 import styles from "./Footer.module.scss";
 import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <footer className={styles.content}>
-      <Link to="/">Hearty plus</Link>
-      
+    <footer className={styles.body}>
+      <div className={styles.inner}>
+        <Link className={styles.logo} to="/">Hearty plus</Link>
+        <ul className={styles.lists}>
+          <li className={styles.list}>
+            <GithubIconSvg />
+          </li>
+          <li className={styles.list}>
+            <InstaIconSvg />
+          </li>
+          <li className={styles.list}>
+            <XIconSvg />
+          </li>
+        </ul>
+      </div>
+      <div className={styles.copyright}>
+        <small>©️ 2024 Hearty plus </small>
+      </div>
     </footer>
-  )
-}
+  );
+};
