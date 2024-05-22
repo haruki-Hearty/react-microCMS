2cc49f39df006f6872ae7ea46a7a1d612bf510d9
haruki <haruki1015go@gmail.com>
2024-05-22 23:16:21 +0900

    [fix]pタグの中にpタグが入れ子になっていたので修正

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,17 +1,17 @@
+import { ConvertText } from "../ConvertText";
 import { DateFormat } from "../DateFormat";
 import styles from "./Card.module.scss";
 
 export const Card = (props) => {
   const { posts } = props;
-  
   return (
     <ul>
       {posts.map((post) => (
         <li key={post.id}>
           <img className={styles.img} src={post.thumbnail.url} alt="" />
-          {DateFormat(post.publishedAt)}
+          <p>{<DateFormat date={post.publishedAt} />}</p>
           <h3>{post.title}</h3>
-          <p>{post.content}</p>
+          {<ConvertText contentHTML={post.content} />}
           <p>{post.category.map((post) => post.name)}</p>
         </li>
       ))}
