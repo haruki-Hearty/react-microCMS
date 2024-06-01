a924db8e213163600c3b614b4f2888ece4614449
haruki <haruki1015go@gmail.com>
2024-06-01 15:18:04 +0900

    [add]最新記事を３件表示する,不要なコード削除

--- src/components/card/WorkCard.jsx
+++ src/components/card/WorkCard.jsx
@@ -1,20 +1,22 @@
 import styles from "./WorkCard.module.scss";
 import { formatYYYYMMDD } from "../../lib/dateFormatter";
-import { truncateText } from "../../lib/truncateText";
 
 export const WorkCard = (props) => {
   const { works } = props;
+  //元の配列を操作しないようにスプレッド構文を使用。
+  const sortedWorks = [...works].sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
+  const latestWorks = sortedWorks.slice(0, 3);
 
   return (
     <ul className={styles.lists}>
-      {works.map((work) => (
+      {latestWorks.map((work) => (
         <li className={styles.list} key={work.id}>
           <img className={styles.img} src={work.thumbnail.url} alt="" />
           <p className={styles.date}>{formatYYYYMMDD(work.publishedAt)}</p>
           <h3 className={styles.title}>{work.company}</h3>
           <div
             className={styles.content}
-            dangerouslySetInnerHTML={{ __html: truncateText(work.request, 40) }}
+            dangerouslySetInnerHTML={{ __html: work.request }}
           ></div>
           <div className={styles.category}>
             {work.category.map((work) => {
