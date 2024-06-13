2fa5d9615db186e1614598f8cc26930abb7503b6
haruki <haruki1015go@gmail.com>
2024-06-13 23:55:51 +0900

    [add]ブログ一覧ページに記事を表示

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,4 +1,22 @@
+import { useBlogPosts } from "../hooks/useBlogPosts";
+import { BlogCard } from "../components/card/BlogCard";
+import styles from "../styles/Blog.module.scss";
+
 const Blog = () => {
-  return <h1>ブログページ</h1>;
+  const { blogs } = useBlogPosts();
+
+  return (
+    <>
+      <h2>ブログ一覧</h2>
+      <div className={styles.cardInner}>
+        <ul className={styles.Cards}>
+          {blogs.map((post) => (
+            <BlogCard key={post.id} post={post} />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 };
+
 export default Blog;
