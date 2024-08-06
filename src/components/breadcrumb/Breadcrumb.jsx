dee8c03ff20fb89978a87830dc5814f13408e39a
haruki <haruki1015go@gmail.com>
2024-08-06 22:20:35 +0900

    [refactor]変数名を変更

--- src/components/breadcrumb/Breadcrumb.jsx
+++ src/components/breadcrumb/Breadcrumb.jsx
@@ -1,18 +1,22 @@
-import { Link, useLocation } from 'react-router-dom';
-
+import { Link, useLocation } from "react-router-dom";
 
 export const Breadcrumb = () => {
   const location = useLocation();
-  const pathName = location.pathname.split('/').filter(x => x);
-  console.log(pathName)
+  const pathNames = location.pathname.split("/").filter((x) => x);
+  console.log(pathNames);
   return (
     <ul>
-      <li><Link to="/">ホーム</Link></li>
-      {pathName.map((value, index) => {
+      <li>
+        <Link to="/">ホーム</Link>
+      </li>
+      {pathNames.map((value, index) => {
+        const to = `/${pathNames.slice(0, index + 1).join('/')}`;
         return (
-          <li key={index}>{value}</li>
-        )
+          <li key={index}>
+            <Link to={to}>{value}</Link>
+          </li>
+        );
       })}
     </ul>
-  )
-}
+  );
+};
