99389529770f8ebcad8851177abd5a8068de5577
haruki <haruki1015go@gmail.com>
2024-08-13 18:12:18 +0900

    [fix]propsを渡していなかったので修正

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -3,22 +3,29 @@ import { WorkCard } from "../components/card/WorkCard";
 import styles from "../styles/Blog.module.scss";
 import { KeyVisual } from "../components/KeyVisual/KeyVisual";
 import { Breadcrumb } from "../components/breadcrumb/Breadcrumb";
+import { ContainerLarge } from "../components/container/ContainerLarge";
 
 const Works = () => {
   const { works } = useWorkPosts();
-
+  const pathNames = [
+    {
+      title: "制作実績一覧",
+    },
+  ];
   return (
     <>
       <KeyVisual title="WORKS" subTitle="sub" img="./works_key_visual.jpg" />
-      <Breadcrumb />
-      <h2>制作実績一覧</h2>
-      <div className={styles.cardInner}>
-        <ul className={styles.Cards}>
-          {works.map((work) => (
-            <WorkCard key={work.id} work={work} />
-          ))}
-        </ul>
-      </div>
+      <ContainerLarge>
+        <Breadcrumb pathNames={pathNames} />
+        <h2>制作実績一覧</h2>
+        <div className={styles.cardInner}>
+          <ul className={styles.Cards}>
+            {works.map((work) => (
+              <WorkCard key={work.id} work={work} />
+            ))}
+          </ul>
+        </div>
+      </ContainerLarge>
     </>
   );
 };
