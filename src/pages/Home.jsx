3d6b6dfed77e231d70390e5288b8e5cc7592113b
haruki <haruki1015go@gmail.com>
2024-09-02 21:01:24 +0900

    [fix]表示崩れの修正

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,22 +6,33 @@ import { useWorkPosts } from "../hooks/useWorkPosts";
 import { useBlogPosts } from "../hooks/useBlogPosts";
 import { TopKeyVisual } from "../components/KeyVisual/TopKeyVisual";
 import { Contact } from "../components/contact/Contact";
+import { Link } from "react-router-dom";
 
 const Home = (props) => {
   const { latestWorks } = useWorkPosts();
-  const { latestBlogs } = useBlogPosts();
+  const { latestBlogs, error } = useBlogPosts();
 
   return (
     <>
       <TopKeyVisual />
-      <SectionTitle japanese="紹介" english="ABOUT" />
+      <div className={styles.titleInner}>
+        <SectionTitle japanese="紹介" english="ABOUT" />
+      </div>
       <div className={styles.cardInner}>
-        <ul className={styles.Cards}>
-          {latestBlogs.map((post) => (
-            <BlogCard key={post.id} post={post} />
-          ))}
-        </ul>
-        <ul className={styles.Cards}>
+        {error ? (
+          <div>{error}</div>
+        ) : (
+          <ul className={styles.cards}>
+            {latestBlogs.map((post) => (
+              <li key={post.id} className={styles.card}>
+                <Link to={`/blog/${post.id}`} className={styles.link}>
+                  <BlogCard post={post} />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+        <ul className={styles.cards}>
           {latestWorks.map((work) => (
             <WorkCard key={work.id} work={work} />
           ))}
